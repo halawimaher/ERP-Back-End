b3989911952d69483f78596eeb397257ad1b30a3
mo-ay <mostafaaanezan@gmail.com>
2021-01-05 05:44:16 +0200

    create all models without relations

---
+++ erp/app/Project.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    //
+    protected $table= 'projects';
+    protected $fillable= [
+        'name'
+    ];
+    public $timestamps = false;
+}
