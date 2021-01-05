6e27c3a86183fadc0cfb51c0eef939e4586bc9d7
mo-ay <mostafaaanezan@gmail.com>
2021-01-05 05:41:10 +0200

    database migration

---
+++ erp/database/migrations/2020_12_29_203859_create_teams_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams_projects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('team_id');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('team_id')->references('id')->on('teams');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams_projects');
+    }
+}
